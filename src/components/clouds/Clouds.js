56bde3606f211fc5606003acea687a4ccf6ca341
mlucchini <marclucchini@gmail.com>
2017-02-26 18:52:19 +0000

    Cloud density is relative to current weather data

--- src/components/clouds/Clouds.js
+++ src/components/clouds/Clouds.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import Isvg from 'react-inlinesvg'
-import clouds from './clouds.svg'
+import cloud from './cloud.svg'
 import './Clouds.css'
 
-const Clouds = () => (
-  <div className="clouds">
-    <Isvg src={clouds} uniquifyIDs={false} />
+const hourToOpacity = (hours) => Math.max(0.2, 1 - Math.abs(12 - hours) / 10)
+
+const Clouds = ({ clouds, hours }) => (
+  isNaN(clouds) ? null :
+  <div className="clouds" style={{ opacity: hourToOpacity(hours) }}>
+  {
+    Array(clouds / 10).fill().map((_, i) =>
+      <div key={`cloud${i}`} className={`cloud${i}`}>
+        <Isvg src={cloud} uniquifyIDs={false} />
+      </div>
+    )
+  }
   </div>
 )
+
+Clouds.propTypes = {
+  clouds: React.PropTypes.number,
+  hours: React.PropTypes.number
+}
 
 export default Clouds
