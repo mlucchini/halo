d589b1258175406df2d745c7a512cfe0b1218a30
mlucchini <marclucchini@gmail.com>
2017-02-27 11:40:53 +0000

    Added menu and images for the README

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Slider from 'material-ui/Slider'
+import './Menu.css'
 
 const Menu = () => (
-  <div id="menu"></div>
+  <MuiThemeProvider muiTheme={getMuiTheme({
+      slider: { trackColor: 'white', selectionColor: 'white' },
+    })}>
+    <div className="menu">
+      <Slider step={1} defaultValue={0} min={0} max={5} className="slider"/>
+    </div>
+  </MuiThemeProvider>
 )
 
 Menu.propTypes = {
