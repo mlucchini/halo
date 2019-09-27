8a242954f23c8a94479291ba80b71c4a3829369f
Marc <marclucchini@gmail.com>
2019-09-27 19:47:46 +0100

    Removed empty lines from PR

--- src/components/trees/Trees.js
+++ src/components/trees/Trees.js
@@ -3,15 +3,11 @@ import Isvg from 'react-inlinesvg'
 import trees from './trees.svg'
 import './Trees.css'
 
-const hasWind = (wind) => wind > 0 && 'wind'
-
-
 const Trees = ({ hours, wind }) => (
-  <span className={hasWind(wind)}>
+  <span className={wind > 0 && 'wind'}>
     <Isvg src={trees} uniquifyIDs={false} className={`hour${hours}`} />
   </span>
 )
-
 
 Trees.propTypes = {
   hours: React.PropTypes.number,
